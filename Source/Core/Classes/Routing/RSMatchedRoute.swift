529177347439f61e05a0150810a9240be0f1574f
jdkizer9 <jdkizer9@users.noreply.github.com>
2018-05-05 22:38:15 -0400

    made properties public

--- Source/Core/Classes/Routing/RSMatchedRoute.swift
+++ Source/Core/Classes/Routing/RSMatchedRoute.swift
@@ -14,8 +14,8 @@ public struct RSMatchedRoute: Equatable {
         return lhs.layout.isEqualTo(rhs.layout) && lhs.match == rhs.match && lhs.route == rhs.route
     }
     
-    let match: RSMatch
-    let route: RSRoute
-    let layout: RSLayout
+    public let match: RSMatch
+    public let route: RSRoute
+    public let layout: RSLayout
     
 }
